5b70a3a10157c207fe0af3d121c566b020a7efcb
Balkrishna Agarwal <krishna@teziapp.com>
2024-12-15 12:34:25 +0530

    Excluding distributed tests

--- part3/test/chat_test.exs
+++ part3/test/chat_test.exs
@@ -2,6 +2,7 @@ defmodule ChatTest do
   use ExUnit.Case, async: true
   doctest Chat
 
+  @tag :distributed
   test "Send message" do
     assert Chat.send_message(:moebi@localhost, "hi") == :ok
   end
